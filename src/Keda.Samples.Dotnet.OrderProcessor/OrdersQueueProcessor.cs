f41386d4e75a30a47741df3dfd25b0fcce401c7f
Tom Kerkhove <kerkhove.tom@gmail.com>
2020-10-12 16:54:24 +0200

    Migrate to new SDK for Service Bus
    Signed-off-by: Tom Kerkhove <kerkhove.tom@gmail.com>

--- src/Keda.Samples.Dotnet.OrderProcessor/OrdersQueueProcessor.cs
+++ src/Keda.Samples.Dotnet.OrderProcessor/OrdersQueueProcessor.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 using Keda.Samples.Dotnet.Contracts;
-using Microsoft.Azure.ServiceBus;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
@@ -16,7 +15,7 @@ namespace Keda.Samples.Dotnet.OrderProcessor
         {
         }
 
-        protected override async Task ProcessMessage(Order order, string messageId, Message.SystemPropertiesCollection systemProperties, IDictionary<string, object> userProperties, CancellationToken cancellationToken)
+        protected override async Task ProcessMessage(Order order, string messageId, IReadOnlyDictionary<string, object> userProperties, CancellationToken cancellationToken)
         {
             Logger.LogInformation("Processing order {OrderId} for {OrderAmount} units of {OrderArticle} bought by {CustomerFirstName} {CustomerLastName}", order.Id, order.Amount, order.ArticleNumber, order.Customer.FirstName, order.Customer.LastName);
 
